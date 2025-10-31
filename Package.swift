bc0f05d589661fab8ec9ec8d130c7044adc089b5
seony42 <seony42@nasmedia.co.kr>
2025-10-31 14:29:27 +0900

    feat: WebKit 명시 추가

--- Package.swift
+++ Package.swift
@@ -1,16 +1,29 @@
 // swift-tools-version: 5.9
+
 import PackageDescription
+
 let package = Package(
     name: "AdMixer",
     platforms: [.iOS(.v13)],
     products: [
-        .library(name: "AdMixer", targets: ["AdMixer"]),
+        .library(
+            name: "AdMixer",
+            targets: ["iOS_SSP_SPM"]
+        ),
     ],
     targets: [
         .binaryTarget(
-            name: "AdMixer",
+            name: "AdMixerBinary",
             url: "https://github.com/Nasmedia-Tech/iOS-AdMixerDownload/raw/main/AdMixer1.1.1-beta.xcframework.zip",
             checksum: "13fc3327610f2ffe854af6b3b96b632d1abe1ff5eb394b5591238024c4736ea5"
+        ),
+        .target(
+            name: "iOS_SSP_SPM",
+            dependencies: ["AdMixerBinary"],
+            path: "Sources/iOS-SSP-SPM",
+            linkerSettings: [
+                .linkedFramework("WebKit")
+            ]
         )
     ]
 )
